f938baad679099d3b1e885c6411df890dd0ef8f6
stijnvancrombrugge <vancrombruggestijn@gmail.com>
2015-09-15 10:07:28 +0200

    MDB

--- ejb-module/src/main/java/com/realdolmen/course/consumer/TextMessageConsumer.java
+++ ejb-module/src/main/java/com/realdolmen/course/consumer/TextMessageConsumer.java
@@ -1,24 +1,46 @@
 package com.realdolmen.course.consumer;
 
 
+import com.realdolmen.course.domain.Ticket;
+
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @MessageDriven(activationConfig = {
         @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:jboss/exported/jms/queue/MyQueue"),
 })
 public class TextMessageConsumer implements MessageListener {
+
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
     public void onMessage(Message message) {
         try {
             TextMessage textMessage = (TextMessage) message;
-            System.out.println("**********************");
-            System.out.println(textMessage.getText());
-            System.out.println("**********************");
+            String[] messages = textMessage.getText().split("\n");
+
+            for (String oneMessage : messages) {
+                String[] splittedMessage = oneMessage.split(",");
+                if (splittedMessage.length == 1) {
+                    Ticket ticket = new Ticket(Integer.parseInt(splittedMessage[0]));
+                    em.persist(ticket);
+                }
+                if (splittedMessage.length == 2) {
+                    Ticket ticket = em.find(Ticket.class, Integer.parseInt(splittedMessage[0]));
+                    ticket.setPrice(Integer.parseInt(splittedMessage[1]));
+                    em.persist(ticket);
+                }
+
+            }
+
+
         } catch (JMSException e) {
             throw new RuntimeException("Deal with this", e);
         }
--- ejb-module/src/test/java/com/realdolmen/course/integration/TextMessageConsumerTest.java
+++ ejb-module/src/test/java/com/realdolmen/course/integration/TextMessageConsumerTest.java
@@ -1,10 +1,11 @@
 package com.realdolmen.course.integration;
 
+import com.realdolmen.course.domain.Ticket;
 import org.junit.Test;
 
 public class TextMessageConsumerTest extends RemoteJmsTest {
     @Test
     public void produceTextMessage() throws Exception {
-        producer.send(session.createTextMessage("Hello World!"));
+        producer.send(session.createTextMessage("1000,10\n30"));
     }
 }
